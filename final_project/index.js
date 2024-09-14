dfc8a6f6be006a4d8bb3c0a575e88331bf34e8da
yuhuiwee <wee.yuhui@gmail.com>
2024-09-14 23:12:41 +0800

    setUp, add authenticator

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+const { use } = require('express/lib/application.js');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -11,7 +12,20 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if(req.session.authorization){
+        let token = req.session.authorization['assessToken'];
+
+        jwt.verify(token, "access", (err, user) =>{
+            if(!err){
+                req.user = user;
+                next();
+            }else{
+                return res.status(403).json({message: "User not authenticated"});
+            }
+        })
+    }else{
+        return res.status(403).json({message: "User not logged in"});
+    }
 });
  
 const PORT =5000;
